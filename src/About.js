eaa2c9790fb114d7ac030f1061be4697c7bda84a
Igwe Naza <igweemmauelchina.gmail.com>
2025-10-02 11:05:57 +0100

    About.js

--- src/About.js
+++ src/About.js
@@ -21,7 +21,6 @@ export default function About() {
 
   return (
     <div>
-     <Navbar/>
       <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
         <div className="absolute inset-0 -z-10 overflow-hidden">
           <svg
